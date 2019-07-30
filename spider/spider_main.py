e28329fc08a8e5936458afbd5053a3791b4f31fb
NieNieBelly <51563305+NieNieBelly@users.noreply.github.com>
2019-07-30 09:44:17 +0800

    Add files via upload

--- spider/spider_main.py
+++ spider/spider_main.py
@@ -1,4 +1,7 @@
 from spider import url_manage, html_downloader, html_parser, html_outputer
+from bs4 import BeautifulSoup
+from urllib import request
+import re
 
 
 class SpiderMain(object):
@@ -7,6 +10,20 @@ class SpiderMain(object):
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
+        self.titles = []
+        self.pictures = []
+        self.links = []
+
+    def dep_craw(self, url):
+        r = request.urlopen(url)
+        html = r.read()
+        soup = BeautifulSoup(html, 'html.parser', from_encoding='GB2312')
+        picture = soup.find('img', src=re.compile(r"https:(.*?).jpg"))
+        picture_link = picture['src']
+        movie_name = soup.find('font', color="#07519a").get_text()
+        link = soup.find('a', href=re.compile(r"ftp://(.*?).mkv"))
+        link = link['href']
+        return movie_name, picture_link, link
 
     def craw(self, root_url):
         count = 1
@@ -17,6 +34,11 @@ class SpiderMain(object):
                 # print('craw %d : %s' % (count, new_url))
                 html_count = self.downloader.download(new_url)
                 new_urls, new_data = self.parser.parse(new_url, html_count)
+                for url in new_urls:
+                    movie_name, picture_link, link = self.dep_craw(url)
+                    self.titles.append(movie_name)
+                    self.pictures.append(picture_link)
+                    self.links.append(link)
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
                 if count == 1000:
@@ -24,13 +46,15 @@ class SpiderMain(object):
                 count = count + 1
             except:
                 print('crawl failed')
-            print(new_data)
             self.outputer.output_html()
+        return self.titles, self.pictures, self.links
 
 
 if __name__ == "__main__":
     for i in range(1, 43):
-        # https://www.dytt8.net/html/tv/rihantv/list_8_2.html
-        root_url = "https://www.dytt8.net/html/tv/rihantv/list_8_"+str(i)+".html"
+        # https://www.dytt8.net/html/gndy/china/list_4_2.html
+        root_url = "https://www.dytt8.net/html/gndy/china/list_4_"+str(i)+".html"
         obj_spider = SpiderMain()
-        obj_spider.craw(root_url)
+        titles, pictures, links = obj_spider.craw(root_url)
+        print(titles, pictures, links)
+
